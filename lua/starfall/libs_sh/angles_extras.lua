bdab5f18adf5ea0023e6e3f822308fca2dd9fd24
Name <adamnejm@gmail.com>
2021-01-30 02:18:21 +0100

    Add Angle.randomize

--- lua/starfall/libs_sh/angles_extras.lua
+++ lua/starfall/libs_sh/angles_extras.lua
@@ -1,4 +1,6 @@
 
+local checkluatype = SF.CheckLuaType
+
 return function(instance)
 	
 	local ang_methods, ang_meta, awrap, unwrap = instance.Types.Angle.Methods, instance.Types.Angle, instance.Types.Angle.Wrap, instance.Types.Angle.Unwrap
@@ -7,6 +9,18 @@ return function(instance)
 	end
 	
 	-- -------------------------------------------
+	
+	--- Randomizes the angle
+	-- Self-modifies
+	-- @param min Number lower limit of each component. Default = -180
+	-- @param max Number upper limit of each component. Default = 180
+	function ang_methods:randomize(min, max, integers)
+		if min then checkluatype(min, TYPE_NUMBER) else min = -180 end
+		if max then checkluatype(max, TYPE_NUMBER) else max = 180 end
+		
+		local rand = math.Rand
+		return wrap { rand(min, max), rand(min, max), rand(min, max) }
+	end
 	
 	--- Normalizes the angle so that each component is constrained within -180..180 range
 	-- Self-modifies
